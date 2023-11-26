d19c0cb2b77f0cba7ce89a1f44944d4e09af1410
Bruno Balula <Bruno Balula@DESKTOP-U5RKPL6>
2023-11-26 14:06:49 -0300

    Update main

--- canvas_project/src/main.ts
+++ canvas_project/src/main.ts
@@ -1,24 +1,10 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { Dot } from './Dot.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas = <HTMLCanvasElement> document.getElementById("canvas");
+const ctx = canvas.getContext("2d")!;
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const dot1 = new Dot(10, 10, 10, 10)
+console.log(dot1)
+dot1.drawDot(ctx);
