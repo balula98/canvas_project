384102e8703a456c3f2996ccd430a3ed4f4088df
balula98 <brunobalula1998@gmail.com>
2023-11-26 23:47:47 -0300

    Update main

--- canvas_project/src/main.ts
+++ canvas_project/src/main.ts
@@ -1,10 +1,7 @@
 import { Dot } from './Dot.ts'
 
 const canvas = <HTMLCanvasElement> document.getElementById("canvas");
-const ctx = canvas.getContext("2d")!;
-const canvasWidth = canvas.width;
-const canvasHeight = canvas.height;
 
-const dot1 = new Dot(10, 10, 10, 10)
-console.log(dot1)
-dot1.drawDot(ctx);
+const dot1 = new Dot(10, 10, 10, 10, canvas)
+
+setInterval(dot1.move, 100)
